88fe98be2d07ceefdb3de8407499631e6da76be3
dewaqintoro <dewa.qintoro@students.amikom.ac.id>
2021-04-21 05:26:22 +0700

    change index

--- pages/index.vue
+++ pages/index.vue
@@ -1,184 +1,112 @@
 <template>
-<!-- <span></span> -->
-<span v-if="!loadingTheme">
-  <Navbar :theme="storeTheme" @changetheme="changetheme" @changesub="changesub" @changeaudio="changeaudio"/>
-  <div class="main" :style="{ background: storeTheme.background, color: storeTheme.color }">
-    <div class="text-center">
-      <input class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="search" v-model="search" @change="searchFilter" placeholder="Cari Surah. . .">
-      <button @click="searchFilter()" class="btn-search text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-        Cari
-      </button>
-    </div>
-    <div class="font-arabic">
-      <div v-if="loading">
-        <Loading />
+<div class="main">
+  <div class="container">
+    <nuxt-link to="/surah" class="box">
+      <div class="content items-center">
+        <img class="m-auto" src="/cooking.png"/>
+        <p>Daftar Surat</p>
       </div>
-      <div v-else>
-        <div class="item" v-for="(surah, index) in pageOfItems" :key="index">
-          <nuxt-link :to="'/surah/'+surah.index">
-            <div class="card">
-              <div class="flex">
-                <div class="idSurah">{{surah.index}}</div>
-                <div class="nameSurah">
-                  <p>{{surah.arabic}}</p>
-                  <p class="mt-4">{{surah.latin}}</p>
-                  <p class="italic text-base">( {{surah.translation}} : {{surah.ayah_count}} ayat )</p>
-                </div>
-              </div>
-            </div>
-          </nuxt-link>
-        </div>
+    </nuxt-link>
+    <nuxt-link to="/surah" class="box">
+      <div class="content items-center">
+        <img class="m-auto" src="/cooking.png"/>
+        <p>Daftar Surat</p>
       </div>
-
-      <div class="text-center py-3">
-				<jw-pagination :items="allSurah" @changePage="onChangePage"></jw-pagination>
-			</div>
-        
-    </div>
+    </nuxt-link>
+    <nuxt-link to="/surah" class="box">
+      <div class="content items-center">
+        <img class="m-auto" src="/cooking.png"/>
+        <p>Daftar Surat</p>
+      </div>
+    </nuxt-link>
+    <nuxt-link to="/surah" class="box">
+      <div class="content items-center">
+        <img class="m-auto" src="/cooking.png"/>
+        <p>Daftar Surat</p>
+      </div>
+    </nuxt-link>
+    <nuxt-link to="/surah" class="box">
+      <div class="content items-center">
+        <img class="m-auto" src="/cooking.png"/>
+        <p>Daftar Surat</p>
+      </div>
+    </nuxt-link>
   </div>
-</span>
+</div>
 </template>
 
-<script>
-import { computed, ref, useAsync, useContext } from '@nuxtjs/composition-api'
-import Navbar from '~/components/quran/Navbar.vue'
-import Loading from '~/components/quran/Loading.vue'
-import json from '~/data/surah-info.json'
-export default {
-  name: 'Quran',
-  components: {
-    Navbar,
-    Loading
-  },
-  setup(_, {emit}){
-    const { app, store } = useContext()
-    const data = json
-    const search = ref('')
-    const allSurah = ref([])
-    const pageOfItems = ref([])
-    const loading = ref(true)
-    const loadingTheme = computed(() => store.state.loadingTheme)
-    const thisTheme = app.$cookies.get('theme')
-    const thisSub = app.$cookies.get('sub')
-    const thisAudio = app.$cookies.get('audio')
-    const initTheme = computed(() => store.state.initTheme)
-    const storeTheme = computed(() => store.state.theme)
-    
-  
-    if(!thisSub){
-      store.dispatch('setSub', 'On')
-    } else {
-      store.dispatch('getSub')
-    }
-    if(!thisAudio){
-      store.dispatch('setAudio', 'On')
-    } else {
-      store.dispatch('getAudio')
-    }
-    if(thisTheme){
-      store.dispatch('getTheme')
-    } else {
-      store.dispatch('setTheme', initTheme.value)
-    }
-    
-    searchFilter()
-    return {
-      search,
-      allSurah,
-      pageOfItems,
-      loading,
-      cek,
-      searchFilter,
-      onChangePage,
-      loadingTheme,
-      changetheme,
-      changesub,
-      changeaudio,
-      storeTheme
-    }
-    async function cek(){
-    }
-    async function changesub(){
-      store.dispatch('changeSub')
-    }
-    async function changeaudio(){
-      store.dispatch('changeAudio')
-    }
-    function changetheme(){
-      store.dispatch('changeTheme')
-    }
-    function onChangePage(data = any){
-      pageOfItems.value = data
-    }
-    function searchFilter(){
-      setTimeout(function () {
-          const result = data.surah_info.filter(surat =>
-            surat.latin.toLowerCase().includes(search.value.toLowerCase())
-          );
-          allSurah.value = result
-          loading.value = false
-      }, 1000);
-      
-    }
-  }
-}
-</script>
 <style lang="postcss" scoped>
+p{color: black;}
 .main {
-  @apply pt-8 min-h-screen;
-}
-@font-face {
-  font-family: "lpmq";
-  src: url(/fonts/lpmq.otf) format("opentype");
-  font-display: swap;
-}
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-.font-arabic{
-  font-family: "lpmq", Arial, sans-serif;
-  line-height: 2;
-}
-.btn-search {
-  background-color: #4497eb;
-}
-.btn-search:hover {
-  background-color: #2187ec;
-}
-.item {
-  @apply px-8 mx-36 my-8;
-}
-.card {
-  @apply text-3xl p-4 rounded-lg;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.15);
-  .idSurah {
-    @apply text-center;
-    width: 50px;
-  }
-  .nameSurah {
-    @apply px-4 text-right w-full;
-  }
+  
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  min-height: 100vh;
+  background-color: #f7f7f7;
   
 }
-.card:hover {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.20);
+.container {
+  position: relative;
+  max-width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(40%, 1fr));
+  grid-template-rows: repeat(minmax(100px, auto));
+  margin: 40px;
+  grid-auto-flow: dense;
+  grid-gap: 20px;
 }
-@screen mobile {
-  .main {
-    @apply pt-16;
-  }
-  .item {
-    @apply mx-2 px-2;
-  }
-  .surat {
-    @apply text-2xl;
-  }
+
+.container .box {
+  @apply rounded-2xl;
+  padding: 20px;
+  display: grid;
+  font-size: 20px;
+  place-items: center;
+  text-align: center;
+  color: #fff;
+  transition: 0.5s;
+
+  background: #f7f7f7;
+box-shadow:  5px 5px 12px #dedede,
+             -5px -5px 12px #ffffff;
 }
+
+.container .box:hover {
+  background: #e91e63;
+}
+
+.container .box img {
+  position: relative;
+  max-width: 50px;
+  margin-bottom: 10px;
+}
+
+
+/* @screen mobile { */
+  .container {
+    margin: 20px;
+  }
+  .container .box:nth-child(1){
+    grid-column: span 2;
+    grid-row: span 1;
+  }
+  .container .box:nth-child(2){
+    grid-column: span 1;
+    grid-row: span 1;
+    
+  }
+  .container .box:nth-child(3){
+    grid-column: span 1;
+    grid-row: span 2;
+    
+  }
+  .container .box:nth-child(4){
+    grid-column: span 1;
+    grid-row: span 2;
+  }
+  .container .box:nth-child(5){
+  }
+  
+/* } */
 </style>
