4aaa6689f4fe2f3ff31ceac120666e77c25e1378
dewaqintoro <dewa.qintoro@students.amikom.ac.id>
2021-04-20 20:55:54 +0700

    init card

---
+++ pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center">
+    Home
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  // props: {
+  //   surah: {
+  //     type: Object,
+  //     required: true,
+  //   },
+  // },
+  setup(){
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
