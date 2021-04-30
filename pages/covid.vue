3d4c9a8dfac77b59559f905cc341ea9e8bcc6e2d
dewaqintoro <dewa.qintoro@students.amikom.ac.id>
2021-04-30 23:27:27 +0700

    indonesia

--- pages/covid.vue
+++ pages/covid.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
     <div class="one">
-      <div class="header font-bold">
+      <div class="header font-bold px-8">
         <div class="text-center text-3xl">Covid</div>
         <!-- <button @click="cek">cek</button> -->
         <p class="text-xl">Statistik</p>
@@ -10,7 +10,7 @@
         <div class="switch">
           <div class="bg-gray-100">
             <div class="box">
-              <input type="checkbox" class="toggle-btn" />
+              <input type="checkbox" class="toggle-btn focus:outline-none " />
               <div class="signup"></div>
               <div class="login"></div>
             </div>
@@ -22,12 +22,12 @@
               <p class="text-2xl font-bold" v-if="indo_Casess.total">{{jumlah_positif}}</p>
               <p>TERKONFIRMASI</p>
             </div>
-            <div class="meninggal text-white text-center py-4 rounded-lg">
+            <div class="aktif text-white text-center py-4 rounded-lg">
               <p class="text-2xl font-bold" v-if="indo_Casess.total">{{jumlah_dirawat.value}}</p>
               <p class="text-xl" v-if="indo_Casess.total">{{jumlah_dirawat.persentase}}%</p>
               <p>KASUS AKTIF</p>
             </div>
-            <div class="meninggal text-white text-center py-4 rounded-lg">
+            <div class="sembuh text-white text-center py-4 rounded-lg">
               <p class="text-2xl font-bold" v-if="indo_Casess.total">{{jumlah_sembuh.value}}</p>
               <p class="text-xl" v-if="indo_Casess.total">{{jumlah_sembuh.persentase}}%</p>
               <p>SEMBUH</p>
@@ -37,13 +37,22 @@
               <p class="text-xl" v-if="indo_Casess.total">{{jumlah_meninggal.persentase}}%</p>
               <p>MENINGGAL</p>
             </div>
-            <div class="meninggal text-white text-center py-4 rounded-lg">VAKSINASI KE-1</div>
-            <div class="meninggal text-white text-center py-4 rounded-lg">VAKSINASI KE-2</div>
+            <div class="vaksinasi text-white text-center py-4 rounded-lg">
+              <p class="text-2xl font-bold" v-if="indo_Vaksinasi.vaksinasi">{{vaksinasi_tahap_1}}</p>
+              <p>VAKSINASI KE-1</p>
+            </div>
+            <div class="vaksinasi text-white text-center py-4 rounded-lg">
+              <p class="text-2xl font-bold" v-if="indo_Vaksinasi.vaksinasi">{{vaksinasi_tahap_2}}</p>
+              <p>VAKSINASI KE-2</p>
+            </div>
             <!-- <div class="tiga bg-blue-500 text-white text-center py-4 rounded-lg sm:col-span-2 md:col-span-1">7</div> -->
           </div>
         </div>
         
       </div>
+    </div>
+    <div class="two p-8">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, temporibus, magnam itaque nihil maiores aspernatur exercitationem pariatur voluptatibus id fuga aliquid, debitis quia odit! Perferendis, facilis rerum fuga consequatur et non magnam! Eius tempora minima, soluta numquam perspiciatis sequi dolorum, repellendus fugit ducimus, iure nihil! Ratione aperiam sint, nesciunt corrupti aut harum at qui veniam ullam nihil culpa reiciendis id, assumenda vero enim ducimus tenetur accusantium in cum odit quam! Excepturi quo totam debitis expedita distinctio molestiae fugit, autem explicabo cupiditate culpa necessitatibus nesciunt accusantium nihil veniam obcaecati nobis atque placeat similique nisi facere maxime? Doloremque eum optio quae consequuntur.
     </div>
   </div>
 </template>
@@ -65,11 +74,44 @@ export default {
     const global_Deaths = ref([])
     const global_Cases = ref({})
     const indo_Casess = ref([])
+    const indo_Vaksinasi = ref([])
+    // const vaksinasi_tahap_1 = computed(() => indo_Vaksinasi.value?.vaksinasi?.total?.jumlah_vaksinasi_1)
+    const vaksinasi_tahap_1 = computed(() => {
+      var bilangan = indo_Vaksinasi.value?.vaksinasi?.total?.jumlah_vaksinasi_1 ;
+      if(typeof bilangan === 'number'){
+        var	number_string = bilangan.toString()
+        let sisa 	= number_string.length % 3
+        let rupiah 	= number_string.substr(0, sisa)
+        let ribuan 	= number_string.substr(sisa).match(/\d{3}/g)
+            
+        if (ribuan) {
+          let separator = sisa ? '.' : '';
+          rupiah += separator + ribuan.join('.');
+        }
+        return rupiah
+      }
+    })
+
+    const vaksinasi_tahap_2 = computed(() => {
+      var bilangan = indo_Vaksinasi.value?.vaksinasi?.total?.jumlah_vaksinasi_2 ;
+      if(typeof bilangan === 'number'){
+        var	number_string = bilangan.toString()
+        let sisa 	= number_string.length % 3
+        let rupiah 	= number_string.substr(0, sisa)
+        let ribuan 	= number_string.substr(sisa).match(/\d{3}/g)
+            
+        if (ribuan) {
+          let separator = sisa ? '.' : '';
+          rupiah += separator + ribuan.join('.');
+        }
+        return rupiah
+      }
+    })
+
 
     const jumlah_positif = computed(() => {
 
       var bilangan = indo_Casess.value?.total?.jumlah_positif ;
-      console.log('bilangan', bilangan)
       var	number_string = bilangan.toString()
       let sisa 	= number_string.length % 3
       let rupiah 	= number_string.substr(0, sisa)
@@ -86,7 +128,6 @@ export default {
     const jumlah_dirawat = computed(() => {
 
       var bilangan = indo_Casess.value?.total?.jumlah_dirawat ;
-      console.log('bilangan', bilangan)
       var	number_string = bilangan.toString()
       let sisa 	= number_string.length % 3
       let rupiah 	= number_string.substr(0, sisa)
@@ -110,7 +151,6 @@ export default {
     const jumlah_sembuh = computed(() => {
 
       var bilangan = indo_Casess.value?.total?.jumlah_sembuh ;
-      console.log('bilangan', bilangan)
       var	number_string = bilangan.toString()
       let sisa 	= number_string.length % 3
       let rupiah 	= number_string.substr(0, sisa)
@@ -134,7 +174,6 @@ export default {
     const jumlah_meninggal = computed(() => {
 
       var bilangan = indo_Casess.value?.total?.jumlah_meninggal ;
-      console.log('bilangan', bilangan)
       var	number_string = bilangan.toString()
       let sisa 	= number_string.length % 3
       let rupiah 	= number_string.substr(0, sisa)
@@ -231,12 +270,15 @@ export default {
     }
     // globalCases()
     indoCases()
-    // vaksinasi()
+    vaksinasi()
     return {
       series,
       chartOptions,
       global_Cases,
       indo_Casess,
+      indo_Vaksinasi,
+      vaksinasi_tahap_1,
+      vaksinasi_tahap_2,
       jumlah_positif,
       jumlah_dirawat,
       jumlah_sembuh,
@@ -251,19 +293,7 @@ export default {
     }
 
     async function cek(){
-      // var bilangan = 23456789;
-	
-      // var	number_string = bilangan.toString()
-      // let sisa 	= number_string.length % 3
-      // let rupiah 	= number_string.substr(0, sisa)
-      // let ribuan 	= number_string.substr(sisa).match(/\d{3}/g)
-          
-      // if (ribuan) {
-      //   let separator = sisa ? '.' : '';
-      //   rupiah += separator + ribuan.join('.');
-      // }
-
-      console.log('jumlah_positif', jumlah_positif.value)
+      console.log('vaksinasi_tahap_1.value', vaksinasi_tahap_1.value)
     }
 
     async function indoCases(){
@@ -281,9 +311,9 @@ export default {
       try{
         const url = `https://ngodingbentar-be.herokuapp.com/api/v1/vaksinasi`
         const result = await axios.get(url);
-        // global_Cases.value = result.data
+        indo_Vaksinasi.value = result.data?.data
 
-        console.log('vaksinasi', result)
+        // console.log('vaksinasi', result.data.data)
       } catch (err){
         console.log('err', err)
       }
@@ -343,8 +373,21 @@ export default {
   background: rgb(253, 166, 4);
 }
 
+.aktif {
+  background: #8e44ad;
+}
+
 .meninggal {
-  background: rgb(223, 43, 43);
+  /* background: rgb(223, 43, 43); */
+  background: #c0392b;
+}
+
+.sembuh {
+  background: #10ac84;
+}
+
+.vaksinasi {
+  background: #74b9ff;
 }
 .toggle-btn {
   -webkit-appearance: none;
@@ -355,6 +398,7 @@ export default {
   top: 12%;
   left: calc(50% - 125px);
   border-radius: 100px;
+  background: #4884b6;
 }
 
 .toggle-btn:before{
@@ -375,9 +419,12 @@ export default {
   left: 50%;
 }
 .one {
-  @apply text-white;
+  @apply text-white ;
+  border-bottom-right-radius: 50px;
+  border-bottom-left-radius: 50px;
   min-height: 300px;
-  background: rgb(48, 6, 126);
+  /* background: #273c75; */
+  background: #346791;
 }
 .case {
   @apply w-full;
