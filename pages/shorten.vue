e79c6d91ecff913c46138bc952ee031cb6e0dc4c
ngodingbentar <dewaqintoro@gmail.com>
2021-07-30 10:16:34 +0700

    shorten - clean up

--- pages/shorten.vue
+++ pages/shorten.vue
@@ -1,59 +1,64 @@
 <template>
-<span>
-  <Navbar :theme="myTheme" />
-  <div class="section">
-    <div class="main">
-      <div class="head">Url Shortener</div>
-      <transition name="toast">
-        <Toast class="text-center m-auto w-full" v-if="showToast" />
-      </transition>
-      <div class="search">
-        <!-- <input class="input-search focus:outline-none" id="username" type="text" v-model="search" placeholder="Paste long url and shorten it."> -->
-        <input class="input-search focus:outline-none" id="username" type="search" v-model="search" @focus="clearSearch" placeholder="Paste long url and shorten it.">
-          <!-- <font-awesome-icon class="iconTheme" :icon="['fas', 'times']" /> -->
-        <button @click="searchFilter()" class="btn-search focus:outline-none" type="button">
-          Shorten
-        </button>
-      </div>
-      <div class="short-url">
-        <input class="copy-text text-center focus:outline-none" :value="myLink.shortUrl || '-'" />
-        <button class="focus:outline-none mx-2 bg-gray-100 py-2 px-4 rounded-lg" @click="copy">
-          Copy
-        </button>
+  <span>
+    <Navbar :theme="myTheme" />
+    <div class="section">
+      <div class="main">
+        <div class="head">Url Shortener</div>
+        <transition name="toast">
+          <Toast v-if="showToast" class="text-center m-auto w-full" />
+        </transition>
+        <div class="search">
+          <input
+            id="username"
+            v-model="search"
+            class="input-search focus:outline-none"
+            type="search"
+            placeholder="Paste long url and shorten it."
+            @focus="clearSearch"
+          >
+          <button class="btn-search focus:outline-none" type="button" @click="searchFilter()">
+            Shorten
+          </button>
+        </div>
+        <div class="short-url">
+          <input :value="myLink.shortUrl || '-'" class="copy-text text-center focus:outline-none">
+          <button class="focus:outline-none mx-2 bg-gray-100 py-2 px-4 rounded-lg" @click="copy">
+            Copy
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-</span>
+  </span>
 </template>
 
 <script>
-import { computed, ref, useAsync, useContext } from '@nuxtjs/composition-api'
-// import Loading from '@/components/Loading.vue'
-// import { VueNotificationList } from '@dafcoe/vue-notification'
+import { ref } from '@nuxtjs/composition-api'
+import axios from 'axios'
 import Toast from '@/components//Toast'
 import Navbar from '~/components/GlobalNavbar'
 
-import axios from 'axios'
 export default {
   name: 'Shorten',
   components: {
     Toast,
     Navbar
   },
-  setup(){
+  setup () {
     const search = ref('')
     const myLink = ref([])
     const myText = ref('wadudu')
     const isCopied = ref(false)
     const showToast = ref(false)
     const triggerToast = () => {
-      showToast.value = true;
-      setTimeout(() => showToast.value = false, 3000)
+      showToast.value = true
+      setTimeout(() => {
+        showToast.value = false
+      }, 3000)
     }
     const myTheme = {
       background: '#088b71',
       color: 'white',
-      boxShadow:  '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      boxShadow: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)'
     }
 
     return {
@@ -62,47 +67,48 @@ export default {
       myText,
       myTheme,
       isCopied,
+      showToast,
       searchFilter,
       cek,
       copy,
       outFunc,
-      showToast,
       triggerToast,
-      clearSearch,
+      clearSearch
     }
 
-    function clearSearch(){
+    function clearSearch () {
       console.log('clearSearch')
     }
 
-    function outFunc() {
-      var tooltip = document.getElementById("myTooltip");
-      tooltip.innerHTML = "Copy to clipboard";
+    function outFunc () {
+      const tooltip = document.getElementById('myTooltip')
+      tooltip.innerHTML = 'Copy to clipboard'
     }
 
-    function copy(){
+    function copy () {
+      // eslint-disable-next-line no-useless-catch
       try {
         navigator.clipboard.writeText(myLink.value?.shortUrl)
+        console.log('copy')
         triggerToast()
-      } catch (e){
+      } catch (e) {
         throw e
       }
     }
-    async function searchFilter(){
+    async function searchFilter () {
       const params = {
-        longUrl : search.value
+        longUrl: search.value
       }
 
-      const url = `https://www.nuxt.my.id/api/v1/shorten`
-      const result = await axios.post(url, params);
+      const url = 'https://www.nuxt.my.id/api/v1/shorten'
+      const result = await axios.post(url, params)
       myLink.value = result?.data
       console.log('result', result)
     }
 
-    function cek(){
+    function cek () {
       console.log('myLink', myLink.value)
     }
-
   }
 }
 </script>
